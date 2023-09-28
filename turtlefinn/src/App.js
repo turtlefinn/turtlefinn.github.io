ed4780f6836b15b8db4873ac87ca3d9e2d351723
Aastha Desai <aasthad123456@gmail.com>
2023-09-28 18:03:05 -0400

    GH pages

--- turtlefinn/src/App.js
+++ turtlefinn/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aastha - Edit <code>src/App.js</code> and save to reload.
+          Aastha2 - Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
